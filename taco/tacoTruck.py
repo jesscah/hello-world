bd55a5d53796364b80ce6b57a3d520d35fb39269
Mike Cramer <imanerd@me.com>
2017-06-13 22:55:34 -0500

    Money changes hands
    A wallet is implemented, and trucks have to purchase stock using their
    wallet

--- taco/tacoTruck.py
+++ taco/tacoTruck.py
@@ -25,12 +25,21 @@ class Emotion(Enum):
 class FoodTruckParticipant:
 	def __init__(self):
 		self.whatIAm = "participant"
+		self.wallet = 0.0
 	
 	def say(self, sayList ):
 		print( self.whatIAm + "\t> " , end='')		
 		for element in sayList:
 			print( element, end='')
 		print()
+		
+	def giveAllowance(self, amount: float):
+		if amount > 0:
+			self.say(("I was given 💵", amount, ". Thanks!"))
+		else:
+			self.say(("I was given 💵", amount, ". Boo!"))
+		self.wallet += amount
+		self.say(("Now I have 💵", self.wallet))
 
 class Customer(FoodTruckParticipant):
 	def __init__(self, name: str):
@@ -38,6 +47,7 @@ class Customer(FoodTruckParticipant):
 		self.emotion = Emotion.hungry
 		self.want = ''
 		self.say(("I am born!"))
+		self.wallet = 0.0
 
 	def wants(self, foodWant: str):
 		self.say(("Do I want a ", foodWant,"?"))
@@ -65,33 +75,53 @@ class Customer(FoodTruckParticipant):
 		self.viewMind()
 
 	def viewMind(self):
-		self.say(( "I'm feeling ", self.emotion, " & want: ", self.want ))
+		self.say(("I have 💵", self.wallet, " & I'm feeling ", self.emotion, " & want: ", self.want ))
 
+	def giveMoney(self, cost: float):
+		givenMoney = 0.0
+		if self.wallet >= cost:
+			self.say(("Here you go"))
+			givenMoney = cost
+			self.wallet -= givenMoney
+		else:
+			self.say(("I don't have enough"))
+		return givenMoney
 
 class FoodTruck(FoodTruckParticipant):
 	def __init__(self, name):
 		self.whatIAm = "🚛 " + name
+		self.wallet = 0.0
 		self.stock = {}
 		self.say(("I am in business!"))
 		self.readStock()
+	
 
-
-	def serveCustomer(self, customer: Customer ):
+	def serveCustomer(self, customer: Customer, cost: float ):
 		self.customerServiceWelcome( customer )
-		self.giveTo( customer )
+		self.transaction( customer, cost )
 		self.readStock()
 		
-	def giveTo(self, customer: Customer):
-		self.say(("Customer wants ",customer.want))
+	def askForMoney(self, customer: Customer, cost: float):
+		self.say(("The price is ", cost))
+		tryToGetMoney = customer.giveMoney(cost)
+		if tryToGetMoney >= cost:
+			self.say(("Here you go"))
+			customer.given( customer.want )
+			self.stock[customer.want] -= 1
+			self.wallet += tryToGetMoney
+		else:
+			self.say(("That wasn't enough"))
+			customer.given('')
+		
+	def transaction(self, customer: Customer, cost):
+		customer.say(("I want a ",customer.want))
 		if customer.want in self.stock:
 			if self.stock[customer.want] > 0:
 				customerPreviousEmotion = customer.emotion
-				self.say(("here you go"))
-				self.stock[customer.want] -= 1
-				customer.given(customer.want)
+				self.askForMoney(customer, cost)
 				if customerPreviousEmotion == Emotion.angry:
-					self.say(("I'm sorry you were feeling Angry, here's another"))
-					self.giveTo(customer)
+					self.say(("I'm sorry you were feeling upset, here's a deal."))
+					self.transaction(customer, cost/2 )
 			else:
 				self.say(("I'm sold out of ", customer.want))
 				customer.given('')
@@ -103,14 +133,19 @@ class FoodTruck(FoodTruckParticipant):
 			customer.given('')
 		
 	def readStock(self):
-		self.say(("I have ", self.stock))
+		self.say(("I have 💵", self.wallet, " and stocked: ", self.stock))
 
-	def stockUpOn(self, foodStuff: str, count: int):
-		self.say(("I'm stocking up on ", foodStuff, " += ", count))
-		if foodStuff in self.stock:
-			self.stock[foodStuff] += count
-		else:
-			self.stock[foodStuff] = count
+	def stockUpOn(self, foodStuff: str, wantPurchase: int, cost: float ):
+		self.say(("I'm stocking up on ", foodStuff, " += ", wantPurchase))
+		if not foodStuff in self.stock:
+			self.stock[foodStuff] = 0
+		havePurchased = 0
+		while	(havePurchased < wantPurchase) & ((self.wallet - cost) >= 0.0):
+			self.stock[foodStuff] += 1
+			self.wallet -= cost
+			havePurchased += 1
+		if havePurchased != wantPurchase:
+			self.say(("I couldn't buy that many, so I got ", havePurchased))	
 		self.readStock()
 			
 	def customerServiceWelcome(self, customer: Customer ):
@@ -123,23 +158,27 @@ class FoodTruck(FoodTruckParticipant):
 
 
 bob = Customer("bob")
+bob.giveAllowance(5.0)
 tacoTruck = FoodTruck("tacoTruck")
-tacoTruck.stockUpOn("taco", 5)
+tacoTruck.giveAllowance(10.0)
+print("---")
+
+tacoTruck.stockUpOn("taco", 6, 2)
 print("---")
 
 bob.wants('burger')
 print("---")
-tacoTruck.serveCustomer(bob)
+tacoTruck.serveCustomer(bob, 3)
 print(">> --- ")
 
 bob.wants('taco')
 print("---")
-tacoTruck.serveCustomer(bob)
+tacoTruck.serveCustomer(bob, 3)
 print(">> ---")
 
 bob.wants('taco')
 print("---")
-tacoTruck.serveCustomer(bob)
+tacoTruck.serveCustomer(bob, 3)
 print(">> ---")
 
 
